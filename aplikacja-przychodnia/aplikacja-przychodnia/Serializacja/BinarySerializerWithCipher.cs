5224a041bbe0eb4c8fab607fb12fd511eacd2b85
Abaddon27 <kacperkru2@gmail.com>
2019-03-24 20:17:52 +0100

    Update BinarySerializerWithCipher.cs
    Zabazpieczenie gdy brak bazy

--- aplikacja-przychodnia/aplikacja-przychodnia/Serializacja/BinarySerializerWithCipher.cs
+++ aplikacja-przychodnia/aplikacja-przychodnia/Serializacja/BinarySerializerWithCipher.cs
@@ -15,35 +15,44 @@ namespace aplikacja_przychodnia
     class BinarySerializerWithCipher 
     {
         public static void Serialize<T>(string filePath, T obj)
-        {            
-            byte[] key = { 1, 2, 3, 4, 5, 6, 7, 8 }; //probowalam dodac opcje z mozliwoscia 
-                                                     //zmiany hasla ale mi sie nie udalo wiec chwilowo zostawiam
-                                                     //w takiej formie (strzelam ze i tak nam to wystarczy)
-            byte[] iv = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        {
+            if (obj != null)
+            {
+                byte[] key = { 1, 2, 3, 4, 5, 6, 7, 8 }; //probowalam dodac opcje z mozliwoscia 
+                                                         //zmiany hasla ale mi sie nie udalo wiec chwilowo zostawiam
+                                                         //w takiej formie (strzelam ze i tak nam to wystarczy)
+                byte[] iv = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
-            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+                DESCryptoServiceProvider des = new DESCryptoServiceProvider();
 
-            using (var fs = new FileStream(filePath, FileMode.Create, FileAccess.Write))
-            using (var cryptoStream = new CryptoStream(fs, des.CreateEncryptor(key, iv), CryptoStreamMode.Write)) {
-                BinaryFormatter formatter = new BinaryFormatter();
-                formatter.Serialize(cryptoStream, obj);
-            }         
+                using (var fs = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+                using (var cryptoStream = new CryptoStream(fs, des.CreateEncryptor(key, iv), CryptoStreamMode.Write))
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    formatter.Serialize(cryptoStream, obj);
+                }
+            }
         }
 
         public static T Deserialize<T>(string filePath) 
         {
-            byte[] key = { 1, 2, 3, 4, 5, 6, 7, 8 };
-            byte[] iv = { 1, 2, 3, 4, 5, 6, 7, 8 };
-
-            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
-
-            using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-            using (var cryptoStream = new CryptoStream(fs, des.CreateDecryptor(key, iv), CryptoStreamMode.Read)) 
+            T temp = default(T);
+            if (File.Exists(filePath))
             {
-                BinaryFormatter formatter = new BinaryFormatter();
-            
-                return (T)formatter.Deserialize(cryptoStream);
+                byte[] key = { 1, 2, 3, 4, 5, 6, 7, 8 };
+                byte[] iv = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+                DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+
+                using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                using (var cryptoStream = new CryptoStream(fs, des.CreateDecryptor(key, iv), CryptoStreamMode.Read))
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+
+                    return (T)formatter.Deserialize(cryptoStream);
+                }
             }
+            return temp;
         }      
     }
 }
