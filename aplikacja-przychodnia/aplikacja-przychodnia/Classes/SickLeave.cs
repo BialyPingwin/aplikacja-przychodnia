f5a591eda46cad4dee9c15d56aee93de5eb1a306
zuzzbi <48810038+zuzzbi@users.noreply.github.com>
2019-04-02 22:04:45 +0200

    Revert "Update SickLeave.cs"
    This reverts commit 31722f7b28f5e7ac582e8c8f6e3d3e3df59f322b.

--- aplikacja-przychodnia/aplikacja-przychodnia/Classes/SickLeave.cs
+++ aplikacja-przychodnia/aplikacja-przychodnia/Classes/SickLeave.cs
@@ -6,9 +6,7 @@ using System.Threading.Tasks;
 
 namespace aplikacja_przychodnia.Classes
 {
-    /// <summary>
-    /// Klasa SickLeave zawiera informacje o wystawianym zwolnieniu.
-    /// </summary>
+    
     [Serializable]
     public class SickLeave
     {
@@ -19,17 +17,8 @@ namespace aplikacja_przychodnia.Classes
         private string _pESEL;
         private DateTime _dateFrom;
         private DateTime _dateTo;
+        
 
-        /// <summary>
-        /// Konstruktor klasy SickLeave zawierający parametry firstName, lastName, sickLeaveType, gender, pESEL, dateFrom, dateTo.
-        /// </summary>
-        /// <param name="firstName">Parametr przechowujący imię pacjenta.</param>
-        /// <param name="lastName">Parametr przechowujący nazwisko pacjenta.</param>
-        /// <param name="sickLeaveType">Parametr przechowujący typ wystawianego zwolnienia.</param>
-        /// <param name="gender">Parametr przechowujący płeć pacjenta.</param>
-        /// <param name="pESEL">Parametr przechowujący numer PESEL pacjenta.</param>
-        /// <param name="dateFrom">Parametr przechowujący datę, od której obowiązuje zwolnienie.</param>
-        /// <param name="dateTo">Parametr przechowujący datę, do której obowiązuje zwolnienie.</param>  
         public SickLeave(string firstName, string lastName, string sickLeaveType, string gender, string pESEL, DateTime dateFrom, DateTime dateTo)
         {
             this.firstName = firstName;
@@ -41,63 +30,42 @@ namespace aplikacja_przychodnia.Classes
             this.dateTo = dateTo;
         }
 
-        /// <summary>
-        /// Właściwość firstName pozwalająca na pobranie wartości parametru _firstName oraz zmiany tej wartości.
-        /// </summary>
         public string firstName
         {
             get { return _firstName; }
             set { _firstName = value; }
         }
 
-        /// <summary>
-        /// Właściwość firstName pozwalająca na pobranie wartości parametru _lastName oraz zmiany tej wartości.
-        /// </summary>
         public string lastName
         {
             get { return _lastName; }
             set { _lastName = value; }
         }
 
-        /// <summary>
-        /// Właściwość firstName pozwalająca na pobranie wartości parametru _sickLeaveType oraz zmiany tej wartości.
-        /// </summary>
         public string sickLeaveType
         {
             get { return _sickLeaveType; }
             set { _sickLeaveType = value; }
         }
 
-        /// <summary>
-        /// Właściwość firstName pozwalająca na pobranie wartości parametru _gender oraz zmiany tej wartości.
-        /// </summary>
         public string gender
         {
             get { return _gender; }
             set { _gender = value; }
         }
 
-        /// <summary>
-        /// Właściwość firstName pozwalająca na pobranie wartości parametru _pESEL oraz zmiany tej wartości.
-        /// </summary>
         public string pESEL
         {
             get { return _pESEL; }
             set { _pESEL = value; }
         }
 
-        /// <summary>
-        /// Właściwość firstName pozwalająca na pobranie wartości parametru _dateFrom oraz zmiany tej wartości.
-        /// </summary>
         public DateTime dateFrom
         {
             get { return _dateFrom; }
             set { _dateFrom = value; }
         }
 
-        /// <summary>
-        /// Właściwość firstName pozwalająca na pobranie wartości parametru _dateTo oraz zmiany tej wartości.
-        /// </summary>
         public DateTime dateTo
         {
             get { return _dateTo; }
